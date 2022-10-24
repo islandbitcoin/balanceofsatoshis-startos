05c5286b9a7eba27f87a49e3e6a247e68271a715
Dread <dread@start9labs.com>
2022-10-24 11:55:37 -0400

    x86

--- scripts/services/healthChecks.ts
+++ scripts/services/healthChecks.ts
@@ -1,5 +1,5 @@
 import { types as T, healthUtil } from "../deps.ts";
 
 export const health: T.ExpectedExports.health = {
-  "web-ui": healthUtil.checkWebUrl("http://balanceofsatoshis.embassy:80")
+  "web-ui": healthUtil.checkWebUrl("http://balanceofsatoshis.embassy:3000")
 }
