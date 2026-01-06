dc2d069ddeb7022c93db982b7de6508bd5d9126f
Sirius <110729325+SiriusLightning21@users.noreply.github.com>
2026-01-06 18:29:06 +0100

    Update migration version to 20.0.1

--- scripts/procedures/migrations.ts
+++ scripts/procedures/migrations.ts
@@ -1,3 +1,3 @@
 import { compat, types as T } from "../deps.ts";
 
-export const migration: T.ExpectedExports.migration = compat.migrations.fromMapping({}, "20.0.0");
+export const migration: T.ExpectedExports.migration = compat.migrations.fromMapping({}, "20.0.1");
